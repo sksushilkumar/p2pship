9cff10e540a643a51c10390439e225800284319e
jookos <jookos@f4f1b6d0-f91c-11de-8cdf-e97ad7d0f28a>
2010-12-29 18:13:12 +0000

    Allow webapi etc addresses to be changed

--- testbed_apps/coffeesipper/p2pship.py
+++ testbed_apps/coffeesipper/p2pship.py
@@ -7,8 +7,8 @@ p2pship_web="localhost:9080"
 p2pship_api="localhost:9081"
 
 
-def get_default_ident():
-    f = urllib.urlopen("http://"+p2pship_web+"/json/idents")
+def get_default_ident(webapi = p2pship_web):
+    f = urllib.urlopen("http://"+webapi+"/json/idents")
     #f = urllib.urlopen("http://localhost:9080/web/start.html")
     s = f.read()
     f.close()
@@ -22,11 +22,13 @@ def get_default_ident():
                 return ident
     return ""
 
-def register_http(service_addr, p2p_addr):
-    f = urllib.urlopen("http://%s/http_register?dport=%d&aor=%s&ttl=-1&url=%s:%d" % (p2pship_api, p2p_addr[1], p2p_addr[0], service_addr[0], service_addr[1]))
+def register_http(service_addr, p2p_addr, api = p2pship_api):
+    url = "http://%s/http_register?dport=%d&aor=%s&ttl=-1&url=%s:%d" % (api, p2p_addr[1], p2p_addr[0], service_addr[0], service_addr[1])
+    print "opening up '%s'" % url
+    f = urllib.urlopen(url)
     s = f.read()
     f.close()
-
+    print "Response to register: %s" % s
 
 if __name__ == "__main__":
     print "default identity is " + get_default_ident()
