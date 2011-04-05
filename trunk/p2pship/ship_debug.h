077b74321b90f83b6924eff3ac33f69e000a0b8a
jookos <jookos@f4f1b6d0-f91c-11de-8cdf-e97ad7d0f28a>
2011-04-05 09:30:17 +0000

    updated macros

--- trunk/p2pship/ship_debug.h
+++ trunk/p2pship/ship_debug.h
@@ -47,46 +47,46 @@ enum {
 #ifdef PRINT_DEBUG
 
 #define LOG_OUT(fmt, args...) \
-    { TUSER_PRINT(fmt, ##args); } 
+	{ TUSER_PRINT(fmt, ##args); } 
 
 #define LOG_HL(fmt, args...) \
-    TUSER_PRINT("[HL] %s: " fmt, __FUNCTION__, ##args)
+	{ TUSER_PRINT("[HL] %s: " fmt, __FUNCTION__, ##args); }
 
 #define LOG_CUSTOM(level, fmt, args...) \
-    if (p2pship_log_level >= level) LOG_OUT("%s " fmt, p2pship_log_labels[level], ##args)
+	{ if (p2pship_log_level >= level) LOG_OUT("%s " fmt, p2pship_log_labels[level], ##args); }
 
 #define LOG_WARN(fmt, args...) \
-    if (p2pship_log_level >= LOG_WARN) LOG_OUT("[WARN] %s: " fmt, __FUNCTION__, ##args)
+	{ if (p2pship_log_level >= LOG_WARN) LOG_OUT("[WARN] %s: " fmt, __FUNCTION__, ##args); }
 
 #define LOG_DEBUG(fmt, args...) \
-    if (p2pship_log_level >= LOG_DEBUG) LOG_OUT("[DEBUG] %s: " fmt, __FUNCTION__, ##args)
+	{ if (p2pship_log_level >= LOG_DEBUG) LOG_OUT("[DEBUG] %s: " fmt, __FUNCTION__, ##args); }
 
 #define LOG_VDEBUG(fmt, args...) \
-    if (p2pship_log_level >= LOG_VDEBUG) LOG_OUT("[VDEBUG] %s: " fmt, __FUNCTION__, ##args)
+	{ if (p2pship_log_level >= LOG_VDEBUG) LOG_OUT("[VDEBUG] %s: " fmt, __FUNCTION__, ##args); }
 
 #define LOG_INFO(fmt, args...) \
-    if (p2pship_log_level >= LOG_INFO) LOG_OUT("[INFO] %s: " fmt, __FUNCTION__, ##args)
+	{ if (p2pship_log_level >= LOG_INFO) LOG_OUT("[INFO] %s: " fmt, __FUNCTION__, ##args); }
 
 #define TODO(fmt, args...) \
-    if (p2pship_log_level >= LOG_ERROR) LOG_OUT("[TODO] %s: " fmt, __FUNCTION__, ##args)
+	{ if (p2pship_log_level >= LOG_ERROR) LOG_OUT("[TODO] %s: " fmt, __FUNCTION__, ##args); }
 
 #define LOG_ERROR(fmt, args...) \
-    if (p2pship_log_level >= LOG_ERROR) LOG_OUT("[ERROR] %s: " fmt, __FUNCTION__, ##args)
+	{ if (p2pship_log_level >= LOG_ERROR) LOG_OUT("[ERROR] %s: " fmt, __FUNCTION__, ##args); }
 
 #define LOG_ASSERT(fmt, args...) \
-    if (p2pship_log_level >= LOG_ERROR) LOG_OUT("[ASSERT] %s: " fmt, __FUNCTION__, ##args)
+	{ if (p2pship_log_level >= LOG_ERROR) LOG_OUT("[ASSERT] %s: " fmt, __FUNCTION__, ##args); }
 
 #else
 
-#define LOG_OUT(fmt, args...)
-#define LOG_HL(fmt, args...)
-#define LOG_WARN(fmt, args...)
-#define LOG_DEBUG(fmt, args...)
-#define LOG_VDEBUG(fmt, args...)
-#define LOG_INFO(fmt, args...)
-#define TODO(fmt, args...)
-#define LOG_ERROR(fmt, args...)
-#define LOG_ASSERT(fmt, args...)
+#define LOG_OUT(fmt, args...) ;
+#define LOG_HL(fmt, args...) ;
+#define LOG_WARN(fmt, args...) ;
+#define LOG_DEBUG(fmt, args...) ;
+#define LOG_VDEBUG(fmt, args...) ;
+#define LOG_INFO(fmt, args...) ;
+#define TODO(fmt, args...) ;
+#define LOG_ERROR(fmt, args...) ;
+#define LOG_ASSERT(fmt, args...) ;
 
 #endif
 
@@ -100,6 +100,11 @@ enum {
 #define ASSERT_ZERO(val, lab) \
 	{ long __tmp = (long)(val); if ((__tmp)) { LOG_ASSERT("Assert failed @ %s:%d, not ZERO (%d)\n", __FILE__, __LINE__, __tmp); goto lab; }}
 
+#define ASSERT_TRUES(val, lab, fmt, args...)				\
+	{ long __tmp = (long)(val); if (!(__tmp)) { LOG_ASSERT("Assert failed @ %s:%d, not TRUE (%d): " fmt, __FILE__, __LINE__, __tmp, ##args); goto lab; }}
+
+#define ASSERT_ZEROS(val, lab, fmt, args...)				\
+	{ long __tmp = (long)(val); if ((__tmp)) { LOG_ASSERT("Assert failed @ %s:%d, not ZERO (%d): " fmt, __FILE__, __LINE__, __tmp, ##args); goto lab; }}
 
 int ship_debug_dump_json(char **msg);
     
